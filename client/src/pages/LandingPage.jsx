8596a5fb9a4b7f13fd6ba66605c67f993a7d7cc7
vedant821 <vedant821@gmail.com>
2026-07-21 15:52:43 +0530

    Remove pricing/subscription plan section from public emergency landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -4,7 +4,6 @@ import Features from '../components/landing/Features';
 import HowItWorks from '../components/landing/HowItWorks';
 import Statistics from '../components/landing/Statistics';
 import Testimonials from '../components/landing/Testimonials';
-import Pricing from '../components/landing/Pricing';
 import FAQ from '../components/landing/FAQ';
 import Footer from '../components/layout/Footer';
 
@@ -17,7 +16,6 @@ export default function LandingPage() {
       <HowItWorks />
       <Statistics />
       <Testimonials />
-      <Pricing />
       <FAQ />
       <Footer />
     </div>
